d372aa15c40be5bdeb425d25457aad74a046aec9
AmarMusic91 <music.amar@hotmail.com>
2021-03-11 23:35:24 +0100

    Ime, prezime, grad

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    
+    var myFirstName = "Amar";
+    var myLastName = "Musić";
+    var myHomeTown = "Sarajevo";
+    
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+    console.log(main());
+    module.exports = main;
